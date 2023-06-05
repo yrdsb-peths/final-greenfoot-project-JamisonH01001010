595467f4f74f4aa16b979447923897da9dc1f2db
JamisonH01001010 <349304303@gapps.yrdsb.ca>
2023-06-05 09:02:42 -0400

    Added coin count to skill menu

--- SkillMenu.java
+++ SkillMenu.java
@@ -12,10 +12,11 @@ public class SkillMenu extends World
     Menu m = new Menu(900, 700);
     
     CoinTracker c = new CoinTracker();
+    Integer cCount = CoinTracker.getCoinCount();
     
     // Text
     GameFont skillMenu = new GameFont("Skill Menu", 900);
-    
+    GameFont numCoins = new GameFont(cCount.toString(), 400);
     public SkillMenu()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -27,5 +28,6 @@ public class SkillMenu extends World
         
         // Text
         addObject(skillMenu, 525, 450);
+        addObject(numCoins, 760, 252);
     }
 }
