de40f15be9f8583ccc9b96414cc75380839b3538
JamisonH01001010 <130993463+JamisonH01001010@users.noreply.github.com>
2023-06-06 21:52:11 -0400

    Added all Tutorial descriptions to world

--- Tutorial.java
+++ Tutorial.java
@@ -15,13 +15,13 @@ public class Tutorial extends World
     // Text
     GameFont tutorial = new GameFont("Tutorial", 300, 160);
     GameFont description1 = new GameFont("Lost Sword is a turn-based RPG: You go first, then the enemy.", 600, 800);
-    GameFont description2 = new GameFont("Each turn you will have 3 skills to choose from, detailed in the Skill Menu.", 600, 970);
-    GameFont description3 = new GameFont("Use the following numbers on your keyboard to use a skill in battle: 1, 2, 3", 100, 1000);
-    GameFont description4 = new GameFont("Skill power scales off HP and ATK, upgradeable in the Skill Menu using tokens", 100, 1000);
-    GameFont description5 = new GameFont("Tokens are obtained through winning battles", 100, 1000);
-    GameFont description6 = new GameFont("All achievements are hidden, decipher the requirements through their name.", 100, 1000); 
-    GameFont description7 = new GameFont("Note: Enemy skills will have no description, adapt accordingly along your journey.", 100, 1000);
-    GameFont description8 = new GameFont("(This Tutorial will be accessable from the Title Screen)", 100, 1000);
+    GameFont description2 = new GameFont("Each turn you will have 3 skills to choose from, detailed in the Skill Menu.", 650, 970);
+    GameFont description3 = new GameFont("Press numbers 1, 2, or 3 on your keyboard to use a skill in battle.", 650, 900);
+    GameFont description4 = new GameFont("Skill power scales off HP and ATK, upgradeable in the Skill Menu using tokens.", 720, 1100);
+    GameFont description5 = new GameFont("Tokens are obtained through winning battles.", 400, 600);
+    GameFont description6 = new GameFont("All achievements are hidden, decipher the requirements through their name.", 650, 1000); 
+    GameFont description7 = new GameFont("Note: Enemy skills have no description, adapt accordingly.", 870, 1000);
+    GameFont description8 = new GameFont("(This Tutorial will be accessable later from the Title Screen)", 350, 800);
     // description 8 will be removed after inital look
     // continue button will be removed and replaced with the home button after initial look
     
@@ -36,12 +36,12 @@ public class Tutorial extends World
         // Text
         addObject(tutorial, 370, 170);
         addObject(description1, 385, 450);
-        addObject(description2, 385, 500);
-        //addObject(description3,
-        //addObject(description4,
-        //addObject(description5,
-        //addObject(description6,
-        //addObject(description7,
-        //addObject(description8,
+        addObject(description2, 390, 520);
+        addObject(description3, 405, 560);
+        addObject(description4, 405, 640);
+        addObject(description5, 400, 525);
+        addObject(description6, 395, 695);
+        addObject(description7, 485, 855);
+        addObject(description8, 395, 720);
     }
 }
