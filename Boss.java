eaa940d5c8a240c657c4e5ab81bba76190f776b8
JamisonH01001010 <349304303@gapps.yrdsb.ca>
2023-06-07 09:15:24 -0400

    Added boss sprite (unimplemented)

---
+++ Boss.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Boss here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Boss extends Actor
+{
+    GreenfootImage[] bossAppear = new GreenfootImage[18];
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
