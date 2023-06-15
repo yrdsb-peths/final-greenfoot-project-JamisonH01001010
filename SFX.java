12fb3c0f5d7261f4b659b1197d9c9c8ad372debf
JamisonH01001010 <130993463+JamisonH01001010@users.noreply.github.com>
2023-06-14 22:59:13 -0400

    added play methods to sfx

--- SFX.java
+++ SFX.java
@@ -11,9 +11,9 @@ public class SFX extends Actor
     static GreenfootSound mcBarrier = new GreenfootSound("shield-guard-6963.mp3");
     static GreenfootSound mcStunSmash = new GreenfootSound("bing1-91919.mp3");
     static GreenfootSound mcDeath = new GreenfootSound("videogame-death-sound-43894.mp3");
-    static GreenfootSound skeleGobDeath = new GreenfootSound("monster-death-grunt-131480.mp3");
-    static GreenfootSound wizardAttack1 = new GreenfootSound("wind-blow-141288.mp3");
-    static GreenfootSound wizardAttack2 = new GreenfootSound("magic-spell-6005.mp3");
+    static GreenfootSound SGDeath = new GreenfootSound("monster-death-grunt-131480.mp3");
+    static GreenfootSound wizardATK1 = new GreenfootSound("wind-blow-141288.mp3");
+    static GreenfootSound wizardATK2 = new GreenfootSound("magic-spell-6005.mp3");
     static GreenfootSound wizardDeath = new GreenfootSound("magic-strike-5856.mp3");
     static GreenfootSound fireKnightAttack2 = new GreenfootSound("fire-magic-6947.mp3");
     static GreenfootSound fireKnightDefend = new GreenfootSound("basic-fire-whoosh-3-104223.mp3");
@@ -47,52 +47,52 @@ public class SFX extends Actor
     public void act()
     {
         if(setClick){
-            
+            click.play();
         }
         if(setNormalAttack){
-            
+            normalAttack.play();
         }
         if(setMCBarrier){
-            
+            mcBarrier.play();
         }
         if(setMCStunSmash){
-            
+            mcStunSmash.play();
         }
         if(setMCDeath){
-            
+            mcDeath.play();
         }
         if(setSGDeath){
-            
+            SGDeath.play();
         }
         if(setWizardATK1){
-            
+            wizardATK1.play();
         }
         if(setWizardATK2){
-            
+            wizardATK2.play();
         }
         if(setWizardDeath){
-            
+            wizardDeath.play();
         }
         if(setFKATK2){
-            
+            fireKnightAttack2.play();
         }
         if(setFKDefend){
-            
+            fireKnightDefend.play();
         }
         if(setFKDeath){
-            
+            fireKnightDeath.play();
         }
         if(setBossATK){
-            
+            bossAttack.play();
         }
         if(setBossConsumeSummon){
-            
+            bossConsumeSummon.play();
         }
         if(setBossDodge){
-            
+            bossDodge.play();
         }
         if(setBossDeath){
-            
+            bossDeath.play();
         }
     }
 }
