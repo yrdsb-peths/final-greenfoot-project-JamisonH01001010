6c7479673c89c05935994c16ad792cd2ed417171
JamisonH01001010 <130993463+JamisonH01001010@users.noreply.github.com>
2023-06-14 20:29:05 -0400

    Added API for overlooked miniicon class

--- MiniIcon.java
+++ MiniIcon.java
@@ -1,17 +1,18 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MiniIcon here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * The mini icon used to construct a themed help button
+ * (As a premade help button was not included in the themed spritesheet)
  */
 public class MiniIcon extends Actor
 {
+    // MiniIcon image
     GreenfootImage m = new GreenfootImage("mini-icon.png");
+    /**
+     * Constructor: Set the image of the miniicon
+     */
     public MiniIcon(){
         m.scale(100, 100);
         setImage(m);
     }
-    
 }
