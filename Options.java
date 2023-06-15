4a3180d459969fb5506d42d7e131b002958454c9
JamisonH01001010 <130993463+JamisonH01001010@users.noreply.github.com>
2023-06-14 20:56:22 -0400

    Fixed music (removed SFX on off button)

--- Options.java
+++ Options.java
@@ -7,7 +7,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Options extends World
 {
     // Count to determine "button" state of options
-    public static int musicCount = 1;
+    public static int musicCount = 0;
     public static int SFXCount = 0;
     // Options Background
     GreenfootImage optionsBG = new GreenfootImage("dungeon2.jpg");
@@ -34,9 +34,6 @@ public class Options extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1, false); 
         
-        
-        
-        
         // Set Options Background
         optionsBG.scale(800, 600);
         setBackground(optionsBG);
@@ -44,11 +41,9 @@ public class Options extends World
         addObject(m, 400, 300);
         // Text
         addObject(options, 460, 330);
-        addObject(music, 400, 360);
-        addObject(soundEffects, 400, 510);
+        addObject(music, 400, 410);
         // Add BlackSquare
-        addObject(b1, 270, 250);
-        addObject(b2, 270, 400);
+        addObject(b1, 270, 300);
         // Add HomeButtion
         addObject(h1, 545, 155);
     }
@@ -64,9 +59,11 @@ public class Options extends World
             musicCount++;
         }
         if(musicCount % 2 == 0){ // music on
-            addObject(c1, 270, 250);
+            addObject(c1, 270, 300);
+            TitleScreen.getBGM().play();
         } else { // music off
             removeObject(c1);
+            TitleScreen.getBGM().pause();
         }
     }
     /**
